40ef2a8ae7b4ee7516f1ded9ecf71678985ab0d1
codesbiome <github@codesbiome.com>
2021-10-15 03:56:09 +0530

    Initial project

---
+++ src/components/Application.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { hot } from 'react-hot-loader';
+import logo from '@assets/images/logo.png';
+import './Application.less';
+
+type Props = {
+  title: string;
+  version: string;
+};
+
+const Application: React.FC<Props> = (props) => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <React.Fragment>
+      <main>
+        <div className='main-heading'>
+          <img src={logo} width='32' title='Codesbiome' />
+          <h1>{props.title}</h1>
+        </div>
+        <p className='main-teaser'>
+          Coming soon...
+        </p>
+      </main>
+    </React.Fragment>
+  );
+};
+
+export default hot(module)(Application);
