f8dae547caf26cf5a244a2c25c71fd5686486db5
codesbiome <github@codesbiome.com>
2021-10-15 05:17:59 +0530

    Export

---
+++ out/_next/static/oC59L6DvIL8D5tsAu9Tq5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1486bb312ba4cb5ae33b.js"],"/_error":["static/chunks/pages/_error-0cd42a815f6ea15e8710.js"],"/about":["static/chunks/pages/about-dffaf3ba708e1120cdbd.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
