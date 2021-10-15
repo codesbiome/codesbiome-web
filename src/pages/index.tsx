e0b1e1f99dd13e01e789fa040966c7cd893bd9ac
codesbiome <github@codesbiome.com>
2021-10-15 05:53:24 +0530

    About link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 
 const HomePage: React.FC = () => (
   <div>
@@ -8,6 +9,10 @@ const HomePage: React.FC = () => (
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
     TBC Next..!
+    <br />
+    <Link href='/about'>
+      <a>About</a>
+    </Link>
   </div>
 );
 
