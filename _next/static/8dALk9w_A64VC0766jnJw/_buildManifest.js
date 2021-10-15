639b149406aad05eed813e5f56dc0e9a31cae403
guasam <72690494+guasam@users.noreply.github.com>
2021-10-15 00:18:36 +0000

    Deploying to gh-pages from @ codesbiome/codesbiome-web@7932dcc13f5a1b1ade85b4217ee2f8a91d01ed51 🚀

--- _next/static/8dALk9w_A64VC0766jnJw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e2fdd3424534b0ed6a80.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],"/about":["static/chunks/pages/about-298078d4fe4436b349cb.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
